0481c77aba41d6ba85bd5b5c7cfaa83e6e3e6cbd
Gaston Arbeletche <garbeletche@vairix.com>
2018-02-08 09:40:38 -0300

    fix error in js custom

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -117,6 +117,7 @@ var initialize_modules = function() {
   App.Sortable.initialize();
   App.TableSortable.initialize();
   App.InvestmentReportAlert.initialize();
+  App.Proposals.initialize();
 };
 
 $(function(){
