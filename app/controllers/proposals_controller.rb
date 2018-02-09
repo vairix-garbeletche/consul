42c3d0de8bca72f2273dab9a2a19631eeddb8f10
Gaston Arbeletche <garbeletche@vairix.com>
2018-02-09 15:48:37 +0100

    Adds setting to allow only admins to create proposals

--- app/controllers/proposals_controller.rb
+++ app/controllers/proposals_controller.rb
@@ -8,6 +8,7 @@ class ProposalsController < ApplicationController
   before_action :load_geozones, only: [:edit, :map, :summary]
   before_action :authenticate_user!, except: [:index, :show, :map, :summary]
   before_action :load_settings
+  before_action :validate_settings, only: [:new, :create]
 
   feature_flag :proposals
 
@@ -134,6 +135,12 @@ class ProposalsController < ApplicationController
 
     def load_successful_proposals
       @proposal_successful_exists = Proposal.successful.exists?
+    end
+
+    def validate_settings
+      unless Proposal.can_manage? current_user
+        redirect_to proposals_path, notice: t('proposals.require_permission')
+      end
     end
 
 end
